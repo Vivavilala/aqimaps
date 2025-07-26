339ba7a3b03842e3f3d10addbe4c749e1d5011ae
Vivaan Vavilala <vivaan.v.vavilala@gmail.com>
2025-07-26 20:59:56 +0530

    Serving up html file sfrom AQIMapServer

--- AQIMapServer.py
+++ AQIMapServer.py
@@ -7,6 +7,18 @@ import requests
 import polyline
 import os
 from dotenv import load_dotenv
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+# Setup template directory
+templates = Jinja2Templates(directory="templates")
+
+# Route to serve frontend
+@app.get("/", response_class=HTMLResponse)
+async def serve_frontend(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 app = FastAPI()
